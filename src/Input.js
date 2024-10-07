d51f02515c685a1c23a0b5a73cc68d2dfae9e6cf
Shadman Ahmed <ShadmanAhmed27@gmail.com>
2024-10-07 14:00:02 -0500

    made an app that can take color input and displays color inside the square as well as give user a hex value for the color

---
+++ src/Input.js
@@ -0,0 +1,21 @@
+import React from "react";
+import colorNames from "colornames";
+
+const Input = ({ colorValue, setColorValue, setHexValue }) => {
+  return (
+    <form onSubmit={(e) => e.preventDefault()}>
+      <label>Add Color Name</label>
+      <input
+        type="text"
+        placeholder="Add Color Name"
+        value={colorValue}
+        onChange={(e) => {
+          setColorValue(e.target.value);
+          setHexValue(colorNames(e.target.value));
+        }}
+      />
+    </form>
+  );
+};
+
+export default Input;
